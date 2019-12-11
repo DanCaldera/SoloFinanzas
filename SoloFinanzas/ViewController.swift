c7a06a430467dc006af3d5702bac10ca48ec0c80
Daniel Caldera <bdcaldera@gmail.com>
2019-12-10 23:00:17 -0600

    Initial Commit

---
+++ SoloFinanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoloFinanzas
+//
+//  Created by Daniel Caldera on 10/12/19.
+//  Copyright © 2019 Daniel Caldera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SoloFinanzasTests/SoloFinanzasTests.swift
@@ -0,0 +1,34 @@
+//
+//  SoloFinanzasTests.swift
+//  SoloFinanzasTests
+//
+//  Created by Daniel Caldera on 10/12/19.
+//  Copyright © 2019 Daniel Caldera. All rights reserved.
+//
+
+import XCTest
+@testable import SoloFinanzas
+
+class SoloFinanzasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
