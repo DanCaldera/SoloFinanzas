a774a0f77da1af251dc4fbc6ba5a8979aa0fc6a6
Daniel Caldera <bdcaldera@gmail.com>
2019-12-13 18:08:45 -0700

    bug00

---
+++ SoloFinanzas/Application/Animation/LottieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LottieViewController.swift
+//  SoloFinanzas
+//
+//  Created by Daniel Caldera on 13/12/19.
+//  Copyright © 2019 Platzi. All rights reserved.
+//
+
+import UIKit
+
+class LottieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
